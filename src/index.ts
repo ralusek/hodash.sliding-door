bad9a90c7cb9e6529a1cc1ffb9e99ab001db78b4
Tomas Savigliano <ralusek@Tomass-MBP.MasterCloset-AP>
2023-04-04 14:40:15 -0700

    Tests passing.

---
+++ src/index.ts
@@ -0,0 +1,86 @@
+type Config = {
+  size?: {
+    min?: number;
+    max?: number;
+  };
+  index: {
+    from?: number;
+    to: number;
+  };
+};
+
+type Payload = {
+  pair: [number, number];
+  iteration: number;
+  currentSize: number;
+  step: number;
+  direction: number;
+  config: Config;
+};
+
+/**
+ * 
+ * For example, if you have 5 items, and you want to perform a memoized series of actions between each item,
+ * where you begin with the base case of 2 items, and then increase the size of the window by 1 each iteration,
+ * passing the memoized results of the previous iteration for reference.
+ * In case described above, iterations would look like this:
+ * 0-1 1-2 2-3 3-4
+ * 0-2 1-3 2-4
+ * 0-3 1-4
+ * 0-4
+ */
+export function slidingWindow<T extends any>(
+  handler: (payload: Payload, memo: T[][]) => T,
+  config: Config,
+) {
+  const index = {
+    from: config.index?.from || 0,
+    to: config.index.to,
+  };
+
+  const range = Math.abs(index.to - index.from);
+
+  const size = {
+    min: config.size?.min || 1,
+    max: config.size?.max || range,
+  };
+
+  const direction = index.from < index.to ? 1 : -1;
+  if (size.min < 1) throw new Error(`Minimum size cannot be less than 2.`);
+  if (size.max < size.min) throw new Error(`Maximum size cannot be less than minimum size.`);
+  if (size.max > range) throw new Error(`Maximum size cannot be greater than range.`);
+
+
+  const memo: T[][] = [];
+
+  const iterations = size.max - size.min + 1;
+
+  for (let i = 0; i < iterations; i++) {
+    const currentSize = size.min + i;
+    const step = currentSize * direction;
+    const last = index.to - (direction * (currentSize - 1));
+
+    for (let j = index.from; j !== last; j += direction) {
+      const y = j + step;
+      const pair: [number, number] = [j, y];
+      const payload: Payload = {
+        pair,
+        iteration: i,
+        currentSize,
+        step,
+        direction,
+        config: {
+          size,
+          index,
+        },
+      };
+
+      memo[j] = memo[j] || [];
+      memo[j][y] = handler(payload, memo);
+    }
+  }
+
+  return memo;
+}
+
+export default slidingWindow;
---
+++ tests/all/01-is-callable.test.ts
@@ -0,0 +1,198 @@
+import 'mocha';
+import { expect } from 'chai';
+
+import { slidingWindow } from '../../lib';
+
+describe('Invocation', () => {
+
+  /**
+     * 0-1 1-2 2-3 3-4
+     * 0-2 1-3 2-4
+     * 0-3 1-4
+     * 0-4
+     */
+  it('should execute basic case as expected.', () => {
+    const config = { size: { min: 1, max: 4 }, index: { from: 0, to: 4 } };
+    const EXPECTED = [
+      {
+        pair: [ 0, 1 ],
+        iteration: 0,
+        currentSize: 1,
+        step: 1,
+        direction: 1,
+      },
+      {
+        pair: [ 1, 2 ],
+        iteration: 0,
+        currentSize: 1,
+        step: 1,
+        direction: 1,
+      },
+      {
+        pair: [ 2, 3 ],
+        iteration: 0,
+        currentSize: 1,
+        step: 1,
+        direction: 1,
+      },
+      {
+        pair: [ 3, 4 ],
+        iteration: 0,
+        currentSize: 1,
+        step: 1,
+        direction: 1,
+      },
+      {
+        pair: [ 0, 2 ],
+        iteration: 1,
+        currentSize: 2,
+        step: 2,
+        direction: 1,
+      },
+      {
+        pair: [ 1, 3 ],
+        iteration: 1,
+        currentSize: 2,
+        step: 2,
+        direction: 1,
+      },
+      {
+        pair: [ 2, 4 ],
+        iteration: 1,
+        currentSize: 2,
+        step: 2,
+        direction: 1,
+      },
+      {
+        pair: [ 0, 3 ],
+        iteration: 2,
+        currentSize: 3,
+        step: 3,
+        direction: 1,
+      },
+      {
+        pair: [ 1, 4 ],
+        iteration: 2,
+        currentSize: 3,
+        step: 3,
+        direction: 1,
+      },
+      {
+        pair: [ 0, 4 ],
+        iteration: 3,
+        currentSize: 4,
+        step: 4,
+        direction: 1,
+      }
+    ];
+
+    let i = 0;
+    const memo = slidingWindow((payload, memo) => {
+      const expected = EXPECTED[i++];
+      expect(payload).to.deep.equal({ ...expected, config });
+
+      return payload.pair.join('-');
+    }, config);
+    expect(i).to.equal(EXPECTED.length);
+
+    EXPECTED.forEach(({ pair }, i) => {
+      expect(memo[pair[0]][pair[1]]).to.equal(pair.join('-'));
+    });
+  });
+
+  /**
+   * 4-3 3-2 2-1 1-0
+   * 4-2 3-1 2-0
+   * 4-1 3-0
+   * 4-0
+   */
+  it('should execute reverse case as expected.', () => {
+    const config = { size: { min: 1, max: 4 }, index: { from: 4, to: 0 } };
+    const EXPECTED = [
+      {
+        pair: [ 4, 3 ],
+        iteration: 0,
+        currentSize: 1,
+        step: -1,
+        direction: -1,
+      },
+      {
+        pair: [ 3, 2 ],
+        iteration: 0,
+        currentSize: 1,
+        step: -1,
+        direction: -1,
+      },
+      {
+        pair: [ 2, 1 ],
+        iteration: 0,
+        currentSize: 1,
+        step: -1,
+        direction: -1,
+      },
+      {
+        pair: [ 1, 0 ],
+        iteration: 0,
+        currentSize: 1,
+        step: -1,
+        direction: -1,
+      },
+      {
+        pair: [ 4, 2 ],
+        iteration: 1,
+        currentSize: 2,
+        step: -2,
+        direction: -1,
+      },
+      {
+        pair: [ 3, 1 ],
+        iteration: 1,
+        currentSize: 2,
+        step: -2,
+        direction: -1,
+      },
+      {
+        pair: [ 2, 0 ],
+        iteration: 1,
+        currentSize: 2,
+        step: -2,
+        direction: -1,
+      },
+      {
+        pair: [ 4, 1 ],
+        iteration: 2,
+        currentSize: 3,
+        step: -3,
+        direction: -1,
+      },
+      {
+        pair: [ 3, 0 ],
+        iteration: 2,
+        currentSize: 3,
+        step: -3,
+        direction: -1,
+      },
+      {
+        pair: [ 4, 0 ],
+        iteration: 3,
+        currentSize: 4,
+        step: -4,
+        direction: -1,
+      }
+    ];
+
+    let i = 0;
+    const memo = slidingWindow((payload, memo) => {
+      const expected = EXPECTED[i++];
+      expect(payload).to.deep.equal({ ...expected, config });
+
+      return payload.pair.join('-');
+    }, config);
+
+    expect(i).to.equal(EXPECTED.length);
+
+    EXPECTED.forEach(({ pair }, i) => {
+      expect(memo[pair[0]][pair[1]]).to.equal(pair.join('-'));
+    });
+  });
+});
